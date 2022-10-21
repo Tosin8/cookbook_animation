9269d49141b1dcde6aa969e2a9e71e20a01c9043
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-22 00:54:31 +0100

    Update animate_physics_simulation.dart

--- lib/projects/animate_physics_simulation.dart
+++ lib/projects/animate_physics_simulation.dart
@@ -6,6 +6,7 @@
 // 1. Setting Up an animation controller
 
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 class PhysicsCardDragDemo extends StatelessWidget {
   const PhysicsCardDragDemo({super.key});
@@ -22,9 +23,9 @@ class PhysicsCardDragDemo extends StatelessWidget {
 }
 
 class DraggableCard extends StatefulWidget {
-  const DraggableCard({super.key, required FlutterLogo child});
+  const DraggableCard({super.key, required this.child});
 
-  final Widget child; 
+  final Widget child;
 
   @override
   State<DraggableCard> createState() => _DraggableCardState();
@@ -34,10 +35,37 @@ class _DraggableCardState extends State<DraggableCard>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
+  // STEP 3: Animate the widget
+  late Animation<Alignment> _animation;
+
+  // CALCUATE AND RNS A SPRING SIMULATION,
+  void _runAnimation(Offset pixelsPerSecond, Size size) {
+    _animation = _controller.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+    // Calculate the velocity relative to the unit interval, [0,1],
+    // used by the animation controller.
+    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
+    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
+    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
+    final unitVelocity = unitsPerSecond.distance;
+    const spring = SpringDescription(
+      mass: 30,
+      stiffness: 1,
+      damping: 1,
+    );
+
+    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
+
+    _controller.animateWith(simulation);
+  }
 
   // STEP 2:  Move the widget using gestures
 
-  Alignment _dragAlignment = Alignment.center; 
+  Alignment _dragAlignment = Alignment.center;
 
   @override
   void initState() {
@@ -54,8 +82,27 @@ class _DraggableCardState extends State<DraggableCard>
 
   @override
   Widget build(BuildContext context) {
-    return const Align(child: Card(
-      child: widget.child, 
-    ));
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+        onPanDown: (details) {},
+        onPanUpdate: (details) {
+          setState(
+            () {
+              _dragAlignment += Alignment(
+                details.delta.dx / (size.width / 2),
+                details.delta.dy / (size.height / 2),
+              );
+            },
+          );
+        },
+        onPanEnd: (details) {
+          _runAnimation(details.velocity.pixelsPerSecond, size);
+        },
+        child: Align(
+          alignment: _dragAlignment,
+          child: Card(
+            child: widget.child,
+          ),
+        ));
   }
 }
