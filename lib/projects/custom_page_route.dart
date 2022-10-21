dd22223b74a4c845d144a4ae1b63cd51d5aed255
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-21 22:18:05 +0100

    Updating with PageRouteBuilder

--- lib/projects/custom_page_route.dart
+++ lib/projects/custom_page_route.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 // to creating a custom page route transition, using the following five steps
 // 1. set up a PageRouteBuilder  2. Create a Tween 3. Add an AnimatedWidget  4. Use a CurveTween  5. Combine the two tweens.
+
+// 1. SETTING UP A PAGEROUTEBUILDER : PageRouteBuilder has two callbacks - one to build the content of the route ( pageBuilder) and one to build the route's transition ( transitionBuilder). And the child parameter in transitionBuilder is teh widget returned from pageBuilder.
+
 class CustomPageRoute extends StatefulWidget {
   const CustomPageRoute({super.key});
 
@@ -12,6 +15,14 @@ class CustomPageRoute extends StatefulWidget {
 class _CustomPageRouteState extends State<CustomPageRoute> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(_createRoute());
+              },
+              child: const Text('Animate Page')),
+        ));
   }
 }
