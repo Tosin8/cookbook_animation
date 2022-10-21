f844657d7261785da71f13fc2770c221d0c5fb59
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-21 21:57:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# cookbook_animation
+
+A new Flutter project.
+
+## Getting Started
+This is a playlist practice of cookbook animation in official flutter web. 
---
+++ lib/projects/custom_page_route.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomPageRoute extends StatefulWidget {
+  const CustomPageRoute({super.key});
+
+  @override
+  State<CustomPageRoute> createState() => _CustomPageRouteState();
+}
+
+class _CustomPageRouteState extends State<CustomPageRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
