a06a326bfd1187953b8fd83dec95a82f71fa4c38
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-21 22:07:47 +0100

    Update custom_page_route.dart

--- lib/projects/custom_page_route.dart
+++ lib/projects/custom_page_route.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// to creating a custom page route transition, using the following five steps
+// 1. set up a PageRouteBuilder  2. Create a Tween 3. Add an AnimatedWidget  4. Use a CurveTween  5. Combine the two tweens.
 class CustomPageRoute extends StatefulWidget {
   const CustomPageRoute({super.key});
 
