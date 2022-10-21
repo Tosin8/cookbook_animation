b7276f3e4ec55fadf9917cb4eb7e6661553bef25
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-21 23:29:47 +0100

    Update animate_physics_simulation.dart

---
+++ lib/projects/animate_physics_simulation.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart'; 
+
+// Physics simulations, making the app interactions to feel realistic and interactive. Eg animating a widget to act as if it were attached to a spring or falling with gravity. 
+
+// USING THE STEPS BELOW TO DEMOSTRATES HOW TO MOVE A WIDGET FROM A DRAGGED POINT BACK TO THE CENTER USING A SPRING SIMULATION. 
+//1. Set up an animation controller  2. Move the widget using gestures  3. Animate the widet   4. Calculate the velocity to simulate a sprining motion. 
+
+
+// 1. Setting Up an animation controller
+
+s
